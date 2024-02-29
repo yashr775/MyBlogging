65d118f7e4d8f0669960c453ced626d87d1dd1f2
yashr775 <rajy29629@gmail.com>
2024-02-29 19:22:44 +0530

    common button created

---
+++ Frontend/src/components/Button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ButtonProps {
+  children: string;
+  type?: "button" | "submit" | "reset"; // Optional prop with limited string values
+  bgColor?: string; // Optional prop for background color
+  textColor?: string; // Optional prop for text color
+  className?: string; // Optional prop for additional class names
+}
+
+function Button({
+  children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
